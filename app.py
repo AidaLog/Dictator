ed722217523238087e0f351ee28bf541d2400b36
eddygulled <edgargulay69@gmail.com>
2023-10-11 18:26:42 +0300

    try gen

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 
 
-st.title("Text to Speech App with Transformers")
+st.title("HugPipe Text Dictator 🎙🎙")
 
 # Input text
 input_text = st.text_area("Enter text for conversion")
@@ -12,10 +12,13 @@ input_text = st.text_area("Enter text for conversion")
 if st.button("Convert to Speech"):
     if input_text:
         # Generate audio
-        output, rate = generate(input_text)
-        st.audio(output, format="audio/wav", sample_rate=rate)
-        # Download audio file
-        st.success("File ready to play and download")
+        try:
+            output, rate = generate(input_text)
+            st.audio(output, format="audio/wav", sample_rate=rate)
+            # Download audio file
+            st.success("File ready to play and download")
+        except:
+            st.error("Sorry, something went wrong. Please reload the page.")
 
     else:
         st.warning("Please enter some text before converting.")
